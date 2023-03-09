46978c8f8eadcb4a00300da4c06c075e80d63675
Configurator <@inbox.ru>
2023-03-10 01:08:15 +0300

    saving

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
 use std::io;
 
 fn main() {
-    println!("rust in..");
+    println!("[rust in..]");
     //
     
     println!("enter:");
     let mut textIn = String::new();
-    let mut numderIn = 0;
     
     io::stdin()
         .read_line(&mut textIn)
         .expect("ERRerr");
     println!("{}", textIn);
+    process( &textIn );
+
+    ownExample( textIn );
+
+    textIn = String::from("new one!!!!!!111");
+    println!("own back: {}", textIn);
     
     //
-    println!("..rust out!");
+    println!("[..rust out!]");
 }
+
+fn process( txt: &String ) {
+    println!("txt: {}", txt);
+    for item in txt.chars() {
+        println!("item: {}", item);
+    }
+}
+
+fn ownExample( txt: String ) {
+    println!("reowned: {}", txt);
+}
+
