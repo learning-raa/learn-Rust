ca856cd22d951e3bb23859b9357ca5e2c92fa12a
AlxAndr R1 <66573649+AlexanderARodin@users.noreply.github.com>
2023-03-11 15:37:37 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,30 @@
 use std::io;
 
-use crate::not_main::*;
-mod not_main;
+//use crate::not_main::*;
+//mod not_main;
 
 fn main() {
     println!("[rust in..]");
     //
+    println!(" 1 - testMacros");
+    println!(" 2 - runThread");
+    println!(" 3 - testOwning");
+    println!("enter task tag:");
     
-    testMacros();
-    runThread();
-
-    println!("enter:");
     let mut textIn = String::new();
-    
     io::stdin()
         .read_line(&mut textIn)
         .expect("ERRerr");
-    println!("{}", textIn);
-    process( &textIn );
     
-    ownExample( textIn );
+    match textIn {
+        "1" => println!("the ONE"),
+        _ => println!("others"),
+    }
     
-    textIn = String::from("new one!!!!!!111");
-    println!("own back: {}", textIn);
+    println!("entered: {}", textIn);
     
+        
     //
     println!("[..rust out!]");
-}
-
-fn process( txt: &String ) {
-    println!("txt: {}", txt);
-    for item in txt.chars() {
-        println!("item: {}", item);
-    }
-}
-
-fn ownExample( txt: String ) {
-    println!("reowned: {}", txt);
 }
 
