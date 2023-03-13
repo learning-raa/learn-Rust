71143d72c8e830ff6e32b53b9ed1ac0b0003e65d
Configurator <@inbox.ru>
2023-03-13 14:02:34 +0300

    saving

--- src/cursive_test.rs
+++ src/cursive_test.rs
@@ -1,3 +1,4 @@
+use cursive::Cursive;
 use cursive::views::{Dialog, TextView};
 
 pub fn testCursive() {
@@ -7,13 +8,25 @@ pub fn testCursive() {
     curRoot.add_global_callback('q', |s| s.quit());
 
     curRoot.add_layer(
-        Dialog::around(TextView::new("wwOww"))
+        Dialog::around(TextView::new("dlg AROUND"))
             .title("rustUI")
-            .button("let Quit", |s| println!("!!!!!!!!!!!!!!!") )
+            .button("show Info", |s| showInfo(s) )
+            .button("var 2", |s| showVar2(s) )
     );
     curRoot.run();
     //
     println!("[..cursive out!]");
 }
 
+fn showInfo(s: &mut Cursive) {
+    s.add_layer(Dialog::info("Some info"));
+}
+
+fn showVar2(s: &mut Cursive) {
+    s.add_layer(
+        Dialog::text("dlg.text")
+            .title("dlg TITLE")
+            .button("press Me", |s| s.add_layer(Dialog::info("nothing")) )
+    );
+}
 
